ccd783527f1db50fc5f14eb3b2409ba21fc22732
Pavel <pavelnikolaichev@gmail.com>
2022-05-21 08:02:15 +0300

    Task 4 completed.

---
+++ Task4.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace src;
+
+class Task4
+{
+    final public function main(string $input): string
+    {
+        $delims = ['_', '-', ' '];
+
+        $input = ucwords(str_replace($delims, ' ', $input), ' ');
+        return str_replace(' ', '', $input);
+    }
+}
