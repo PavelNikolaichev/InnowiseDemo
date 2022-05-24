7d9220a21558b527028125f3f89b49232ee79b9b
Pavel <pavelnikolaichev@gmail.com>
2022-05-24 22:08:20 +0300

    Codestyle fixes.

--- Task9.php
+++ Task9.php
@@ -6,11 +6,11 @@ class Task9
 {
     final public function main(array $arr, int $number): array
     {
-        $result=[];
+        $result = [];
 
         for ($x = 0; $x < count($arr) - 2; $x++) {
-            if ($arr[$x] + $arr[$x+1] + $arr[$x+2] === $number) {
-                $result[] = "{$arr[$x]} + {$arr[$x+1]} + {$arr[$x+2]} = {$number}";
+            if ($arr[$x] + $arr[$x + 1] + $arr[$x + 2] === $number) {
+                $result[] = "{$arr[$x]} + {$arr[$x + 1]} + {$arr[$x + 2]} = {$number}";
             }
         }
 
