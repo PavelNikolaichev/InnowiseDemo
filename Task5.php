edba75bbdc96dd66445935baf4d74b3c01b932e4
Pavel <pavelnikolaichev@gmail.com>
2022-05-24 20:31:19 +0300

    Something that reminds me of a solution(That is a bruteforce, lol).

--- Task5.php
+++ Task5.php
@@ -4,26 +4,46 @@ namespace src;
 
 class Task5
 {
-    final public function numberOfDigits($n): int
+    final public function getLen(int $n): int
     {
-//        $phi = (1+sqrt(5))/2;
-//
-//        if ($n == 1)
-//            return 1;
+        if ($n === 1) {
+            return 1;
+        }
 
-        // Get length of the n-th Fibonacci number
-//        $d = $n * log10($phi) - log10(sqrt(5)) / 2;
+        $phi = (1 + sqrt(5)) / 2;
 
-        // Get the n-th Fibonacci number from the length. TODO: implement it.
-//        echo ($d + log10(sqrt(5)) / 2) / log10($phi) . PHP_EOL;
-//
-//        return ceil($d);
+        return ceil($n * log10($phi) - log10(sqrt(5)) / 2);
+    }
 
-        throw new \Exception('Not implemented yet');
+    final public function numberOfDigits(int $n): int
+    {
+        $cursor = $n*3;
+
+        while ($cursor > 0) {
+            if ($this->getLen($cursor) === $n) {
+                if ($this->getLen($cursor - 1) !== $n) {
+                    return $this->getFibonacci($cursor+1);
+                }
+
+                $cursor--;
+            } elseif ($this->getLen($cursor) < $n) {
+                $cursor++;
+            } else {
+                $cursor--;
+            }
+        }
+
+        return 0;
     }
 
     final public function main(int $n): int
     {
         return $this->numberOfDigits($n);
+    }
+
+    private function getFibonacci(int $n): int
+    {
+        $phi = (1 + sqrt(5)) / 2;
+        return round($phi ** $n / sqrt(5));
     }
 }
