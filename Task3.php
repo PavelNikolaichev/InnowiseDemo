b468169b909aed8d553101cebdef98d92d55625d
Pavel <pavelnikolaichev@gmail.com>
2022-05-25 16:50:46 +0300

    Added input checks.

--- Task3.php
+++ Task3.php
@@ -2,12 +2,17 @@
 
 namespace src;
 
+use InvalidArgumentException;
+
 class Task3
 {
     final public function main(int $inputNumber): int
     {
+        if (strlen($inputNumber) === 1) {
+            throw new InvalidArgumentException('Input number must have more than 1 digit and be an integer');
+        }
         if ($inputNumber < 0) {
-            $inputNumber = abs($inputNumber);
+            throw new InvalidArgumentException('Input number must be positive');
         }
 
         while (strlen($inputNumber) > 1) {
