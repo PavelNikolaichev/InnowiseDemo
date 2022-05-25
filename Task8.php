5d837094c8259f9d22bc03476bee5be745522a26
Pavel <pavelnikolaichev@gmail.com>
2022-05-25 22:13:52 +0300

    Fixed the bug and added simple JSON checking(hope this one will work).

--- Task8.php
+++ Task8.php
@@ -9,19 +9,24 @@ class Task8
     final public function main(string $json): string
     {
         $data = json_decode($json, true, 512, JSON_THROW_ON_ERROR | JSON_OBJECT_AS_ARRAY);
-        $parsed = '';
+
+        if (!is_array($data)) {
+            throw new InvalidArgumentException('Invalid JSON');
+        }
+
+        $parsed = [];
 
         foreach ($data as $key => $value) {
             if (is_array($value)) {
-                $parsed .= $this->main(json_encode($value, JSON_THROW_ON_ERROR));
+                $parsed[] = $this->main(json_encode($value, JSON_THROW_ON_ERROR));
             } else {
                 if (is_int($value)) {
                     throw new InvalidArgumentException('Invalid value type');
                 }
-                $parsed .= $key . ': ' . $value . PHP_EOL;
+                $parsed[] = $key . ': ' . $value;
             }
         }
 
-        return $parsed;
+        return implode(PHP_EOL, $parsed);
     }
 }
