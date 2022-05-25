c5f79bda094a344bb02f926a4247ac582af4dc58
Pavel <pavelnikolaichev@gmail.com>
2022-05-25 18:10:05 +0300

    Added some input checks and fixed count bug.

--- Task6.php
+++ Task6.php
@@ -4,12 +4,23 @@ namespace src;
 
 class Task6
 {
-    final public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): array
+    final public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
     {
+        if ($year < $lastYear) {
+            throw new \InvalidArgumentException('Year must be lower than last year');
+        }
+        if ($year < 0) {
+            throw new \InvalidArgumentException('Year must be positive');
+        }
+        if ($lastYear < 0) {
+            throw new \InvalidArgumentException('Last year must be positive');
+        }
+
+
         $startdate = strtotime($year . '-' . $month . '-' . '01');
         $enddate = strtotime($lastYear . '-' . $lastMonth . '-' . '01');
 
-        return $this->findMondays($day, $startdate, $enddate);
+        return count($this->findMondays($day, $startdate, $enddate));
     }
 
     private function findMondays(string $day, ?int $startdate, ?int $enddate, array $dates = []): array
