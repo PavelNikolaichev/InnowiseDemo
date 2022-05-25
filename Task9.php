d051aafd3dec5fcfb56e8c5a58255c2282b53404
Pavel <pavelnikolaichev@gmail.com>
2022-05-25 18:33:50 +0300

    Added some input checks and fixed a bug with dimensions.

--- Task9.php
+++ Task9.php
@@ -2,16 +2,32 @@
 
 namespace src;
 
+use InvalidArgumentException;
+
 class Task9
 {
     final public function main(array $arr, int $number): array
     {
+        if ($number === 0) {
+            throw new InvalidArgumentException('Number should be higher than 0');
+        }
+
+        foreach ($arr as $value) {
+            if ($value < 0) {
+                throw new InvalidArgumentException('Array elements should be positive');
+            }
+        }
+
         $result = [];
 
         for ($x = 0; $x < count($arr) - 2; $x++) {
             if ($arr[$x] + $arr[$x + 1] + $arr[$x + 2] === $number) {
-                $result[] = "{$arr[$x]} + {$arr[$x + 1]} + {$arr[$x + 2]} = {$number}";
+                $result[] = ["{$arr[$x]} + {$arr[$x + 1]} + {$arr[$x + 2]} = {$number}"];
             }
+        }
+
+        if (count($result) === 0) {
+            throw new InvalidArgumentException('Sum is too high to find');
         }
 
         return $result;
