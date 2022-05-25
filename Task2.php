8b6585c851700e114eceea980735199594481308
Pavel <pavelnikolaichev@gmail.com>
2022-05-25 18:47:43 +0300

    Style-fixes. Again...

--- Task2.php
+++ Task2.php
@@ -2,15 +2,17 @@
 
 namespace src;
 
+use InvalidArgumentException;
+
 class Task2
 {
     final public function main(string $date): int
     {
-        $format = "d.m.Y";
+        $format = 'd.m.Y';
         $timed_date = date_create_from_format($format, $date);
 
         if (strtotime($date) === false) {
-            throw new \InvalidArgumentException("Wrong date format. Use: $format");
+            throw new InvalidArgumentException("Wrong date format. Use: $format");
         }
 
         return date_diff(date_create_from_format($format, date($format)), $timed_date)->d;
