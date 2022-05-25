0372895f1e4bef3b7db8a052fa754b50e468cd2c
Pavel <pavelnikolaichev@gmail.com>
2022-05-25 18:46:38 +0300

    A huge refactoring.

--- Task12.php
+++ Task12.php
@@ -2,12 +2,86 @@
 
 namespace src;
 
-require_once 'MyCalculator.php';
+use InvalidArgumentException;
 
 class Task12
 {
-    final public function main()
+    private int $a;
+    private int $b;
+
+    public function __construct(int $a, int $b)
     {
-        throw new \Exception('Not implemented');
+        if ($a < 0 || $b < 0) {
+            throw new InvalidArgumentException('Input values must be positive');
+        }
+
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    final public function add(): Task12
+    {
+        return new Task12($this->a + $this->b, $this->b);
+    }
+
+    final public function addBy(int $num): Task12
+    {
+        if ($num < 0) {
+            throw new InvalidArgumentException('Input values must be positive');
+        }
+
+        return new Task12($this->a + $num, $this->b);
+    }
+
+    final public function subtract(): Task12
+    {
+        return new Task12($this->a - $this->b, $this->b);
+    }
+
+    final public function subtractBy(int $num): Task12
+    {
+        if ($num < 0) {
+            throw new InvalidArgumentException('Input values must be positive');
+        }
+
+        return new Task12($this->a - $num, $this->b);
+    }
+
+    final public function multiply(): Task12
+    {
+        return new Task12($this->a * $this->b, $this->b);
+    }
+
+    final public function multiplyBy(int $num): Task12
+    {
+        if ($num < 0) {
+            throw new InvalidArgumentException('Input values must be positive');
+        }
+
+        return new Task12($this->a * $num, $this->b);
+    }
+
+    final public function divide(): Task12
+    {
+        return new Task12($this->a / $this->b, $this->b);
+    }
+
+    final public function divideBy(int $num): Task12
+    {
+        if ($num < 0) {
+            throw new InvalidArgumentException('Input values must be positive');
+        }
+
+        return new Task12($this->a / $num, $this->b);
+    }
+
+    final public function __toString(): string
+    {
+        return $this->a;
+    }
+
+    final public function main(): void
+    {
+        throw new \Exception('Not implemented, this method is unnecessary');
     }
 }
