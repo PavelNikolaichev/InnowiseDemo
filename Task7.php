8c792a4e960fdf0acfb08c580c7d85eb78546160
Pavel <pavelnikolaichev@gmail.com>
2022-05-25 18:12:36 +0300

    Added some input checks.

--- Task7.php
+++ Task7.php
@@ -6,6 +6,13 @@ class Task7
 {
     final public function main(array $arr, int $position): array
     {
+        if ($position < 0 || $position >= count($arr)) {
+            throw new \InvalidArgumentException('Position is out of array range');
+        }
+        if ($arr === []) {
+            throw new \InvalidArgumentException('Array is empty');
+        }
+
         array_splice($arr, $position, 1);
 
         return $arr;
