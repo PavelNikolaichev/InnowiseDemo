f18acaa79ba60f65c283234b6dd49a28920f7fe0
Pavel <pavelnikolaichev@gmail.com>
2022-05-25 21:40:29 +0300

    Bugfix.

--- Task2.php
+++ Task2.php
@@ -8,13 +8,22 @@ class Task2
 {
     final public function main(string $date): int
     {
-        $format = 'd.m.Y';
+        $format = 'd-m-Y';
         $timed_date = date_create_from_format($format, $date);
+        $curr_date = date_create_from_format($format, date($format));
 
-        if (strtotime($date) === false) {
+        if ($timed_date === false) {
             throw new InvalidArgumentException("Wrong date format. Use: $format");
         }
 
-        return date_diff(date_create_from_format($format, date($format)), $timed_date)->d;
+        if (!checkdate($timed_date->format('m'), $timed_date->format('d'), $timed_date->format('Y'))) {
+            throw new InvalidArgumentException("Wrong date format. Use: $format");
+        }
+
+        if ($curr_date > $timed_date) {
+            $timed_date = date_create_from_format($format, date($format, strtotime('+1 year', $timed_date->getTimestamp())));
+        }
+
+        return $timed_date->diff($curr_date)->days;
     }
 }
