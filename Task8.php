4fab7a6846f9d3d8b7d8f536342889fce2f33342
Pavel <pavelnikolaichev@gmail.com>
2022-05-25 19:29:57 +0300

    Fixed dimension problem and changed the output.

--- Task8.php
+++ Task8.php
@@ -2,12 +2,26 @@
 
 namespace src;
 
+use InvalidArgumentException;
+
 class Task8
 {
-    final public function main(string $json): array
+    final public function main(string $json): string
     {
         $data = json_decode($json, true, 512, JSON_THROW_ON_ERROR | JSON_OBJECT_AS_ARRAY);
+        $parsed = '';
 
-        return $data;
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                $parsed .= $this->main(json_encode($value, JSON_THROW_ON_ERROR));
+            } else {
+                if (is_int($value)) {
+                    throw new InvalidArgumentException('Invalid value type');
+                }
+                $parsed .= $key . ': ' . $value . '\r\n';
+            }
+        }
+
+        return $parsed;
     }
 }
